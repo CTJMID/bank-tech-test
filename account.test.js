992db2be2c880530cc31cc96b7b512e16bbc842c
Chris Middleton <ctjmiddleton@gmail.com>
2022-03-28 12:16:22 +0100

    Refactored  tests using beforeEath method

--- account.test.js
+++ account.test.js
@@ -1,48 +1,51 @@
 const Account = require('./account')
 
 describe('Account', () => {
-    it('Should start with a balanace of zero', () => {
-        const account = new Account();
 
-        expect(account.balance).toEqual(0);
-    })
+    let account;
 
-    it('Should have an increased balance when deposit is made', ()  =>  {
-        const account = new Account();
-        account.depositFunds(100)
+    beforeEach(() => {
+       account = new Account();
+      });
 
-        expect(account.balance).toEqual(100);
-    })
+    describe ('depositFund', () => {
+        it('Should start with a balanace of zero', () => {
 
-    it('Should be able to increase balance by different amounts when alternative deposits are made', ()  =>  {
-        const account = new Account();
-        account.depositFunds(200);
+            expect(account.balance).toEqual(0);
+        })
 
-        expect(account.balance).toEqual(200);
-    })
+        it('Should have an increased balance when deposit is made', ()  =>  {
+            account.depositFunds(100)
 
-    it('Should be able to increase balance by different amounts when a third different deposit is made ', ()  =>  {
-        const account = new Account();
-        account.depositFunds(300)
+            expect(account.balance).toEqual(100);
+         })
 
-        expect(account.balance).toEqual(300);
-    })
+        it('Should be able to increase balance by different amounts when alternative deposits are made', ()  =>  {
+            account.depositFunds(200);
 
-    it('Should be able to add multiple deposits', ()  =>  {
-        const account = new Account();
-        account.depositFunds(300)
-        account.depositFunds(400)
+            expect(account.balance).toEqual(200);
+        })
 
-        expect(account.balance).toEqual(700);
-    })
+        it('Should be able to increase balance by different amounts when a third different deposit is made ', ()  =>  {
+            account.depositFunds(300)
 
-    it('Should be able to add multiple deposits with alternative amounts', ()  =>  {
-        const account = new Account();
-        account.depositFunds(300)
-        account.depositFunds(400)
-        account.depositFunds(500)
+            expect(account.balance).toEqual(300);
+        })
 
-        expect(account.balance).toEqual(1200);
+        it('Should be able to add multiple deposits', ()  =>  {
+            account.depositFunds(300)
+            account.depositFunds(400)
+
+            expect(account.balance).toEqual(700);
+        })
+
+        it('Should be able to add multiple deposits with alternative amounts', ()  =>  {
+            account.depositFunds(300)
+            account.depositFunds(400)
+            account.depositFunds(500)
+
+            expect(account.balance).toEqual(1200);
+        })
     })
 
 })
