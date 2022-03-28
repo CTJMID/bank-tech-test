cafb49281e166527ada4a3af6b8a17d57a617a65
Chris Middleton <ctjmiddleton@gmail.com>
2022-03-28 15:28:23 +0100

    Refactored transction test suite using a beforeeach - all tests  pass

--- transaction.test.js
+++ transaction.test.js
@@ -2,30 +2,27 @@ const Transaction  = require('./transaction')
 
 describe('Trasaction',  () => {
 
-    it('stores the date of the transaction', () => {
+    let transaction;
+    beforeEach(() => {
+       transaction = new Transaction('1/1/2022', 100, 200, 300);
+      });
 
-        transaction = new Transaction('1/1/2022', 100, 200, 300)
+    it('stores the date of the transaction', () => {
 
         expect(transaction.date).toEqual('1/1/2022')
     })
 
     it('stores the credit amount of the transaction', () => {
 
-        transaction = new Transaction('1/1/2022', 100, 200, 300)
-
         expect(transaction.credit).toEqual(100)
     })
 
     it('stores the debit amount of the transaction', () => {
 
-        transaction = new Transaction('1/1/2022', 100, 200, 300)
-
         expect(transaction.debit).toEqual(200)
     })
 
-    it('stores the debit amount of the transaction', () => {
-
-        transaction = new Transaction('1/1/2022', 100, 200, 300)
+    it('stores the balance amount of the transaction', () => {
 
         expect(transaction.balance).toEqual(300)
     })
