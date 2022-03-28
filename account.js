a525fd44431acfe8cd3a0cf752cca1ac9d574ba5
Chris Middleton <ctjmiddleton@gmail.com>
2022-03-28 11:52:25 +0100

    refactor depositFunds method

--- account.js
+++ account.js
@@ -6,13 +6,7 @@
       }
 
     depositFunds(amount) {
-        if(amount === 200){
-            this.balance = 200;
-        } else if(amount === 300) {
-            this.balance = 300;
-        } else {
-            this.balance = 100
-        }
+        this.balance = amount
     }
 
 
