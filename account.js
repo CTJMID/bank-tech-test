9e66b8920d9aa1950c78875ffd949793310cbfc2
Chris Middleton <ctjmiddleton@gmail.com>
2022-03-28 12:00:23 +0100

    refactor depositFunds method to deal with adding multiple different amounts

--- account.js
+++ account.js
@@ -6,13 +6,7 @@
       }
 
     depositFunds(amount) {
-        if( amount === 400 ){
-            this.balance = 700
-        } else if ( amount === 500 ){
-            this.balance = 1200
-        } else {
-            this.balance = amount
-        }
+        this.balance += amount
     }
 
 
