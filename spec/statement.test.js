edad3614fe9437226c596e88804181617ba736ab
Chris Middleton <ctjmiddleton@gmail.com>
2022-03-30 16:20:02 +0100

    revised statemnet testing suite with use of doubles

--- spec/statement.test.js
+++ spec/statement.test.js
@@ -3,12 +3,14 @@ const Statement = require('../lib/statement')
 global.console = {
     log: jest.fn(),
 }
+let header =  'date || credit || debit || balance\n'
 
 describe('Statement', () => {
 
     it('returns a header when called with empty transaction history', () => {
         const statement = new Statement([])
-        expect(statement.formatHeader()).toEqual('date || credit || debit || balance\n')
+        expect(statement.formatHeader()).toEqual(`${header}`)
+        expect(global.console.log).toHaveBeenCalledWith(`${header}`)  
     })
 
     it('takes the history and prints them on different lines', ()  => {
@@ -24,8 +26,16 @@ describe('Statement', () => {
           )  
     })
 
-
-
+    it('makes sure that the convert method chains to printFinalStatement method', () => {
+        const transcationDouble1 = { formatTransaction: () => 'a' };
+        const transcationDouble2 = { formatTransaction: () => 'b' };
+        const transcationDouble3 = { formatTransaction: () => 'c' };
+        const statement = new Statement([transcationDouble1, transcationDouble2, transcationDouble3])
+        statement.convert()
+        expect(global.console.log).toHaveBeenCalledWith(`${header}c\nb\na\n`)  
+    })
 })
+
+
 
 
